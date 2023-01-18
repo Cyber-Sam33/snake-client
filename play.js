206cac1f0c3158296443d2b25732995d19162700
Cyber-Sam33 <issasammi@yahoo.com>
2023-01-18 22:35:25 +0000

    update to play.js

--- play.js
+++ play.js
@@ -2,7 +2,5 @@ const net = require("net");
 const { connect } = require('./client'); // works as varaible name match keys - see connection module (W2D3)
 const { setupInput } = require('./input.js');
 
-
-
 console.log("Connecting ...");
 setupInput(connect());
